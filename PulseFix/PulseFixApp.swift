bf11f52bc53a9e3f025ef0b8114aad03236cf9f6
Michael Hany <MichaelHany101@.github.com>
2026-09-19 14:56:48 +0300

    Initial Commit

---
+++ PulseFix/PulseFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseFixApp.swift
+//  PulseFix
+//
+//  Created by Michael Hany on 19/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PulseFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
